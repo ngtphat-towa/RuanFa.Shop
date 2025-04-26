29ce816d069b36ba36bd2a75c29aac54caff692f
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2025-04-26 09:48:53 +0700

    feat(todo): Enhance TodoItem and related structures
    Updated TodoItem with properties and methods for management, added PriorityLevel enum, and introduced error handling classes for TodoItem and TodoList. Added event classes for TodoItem lifecycle and implemented Colour value object with supported colors.

---
+++ src/Domain/Todo/Errors/TodoItemErorrs.cs
@@ -0,0 +1,20 @@
+﻿using ErrorOr;
+
+namespace RuanFa.Shop.Domain.Todo.Errors;
+public static partial class DomainErrors
+{
+    public static class TodoItem
+    {
+        public static Error InvalidTitle => Error.Failure(
+                   code: "TodoItem.InvalidTitle",
+                   description: "The title is invalid.");
+
+        public static Error InvalidPriority => Error.Failure(
+            code: "TodoItem.InvalidPriority",
+            description: "The priority value is invalid.");
+
+        public static Error InvalidListId => Error.Failure(
+           code: "TodoItem.InvalidListId",
+           description: "The ListId is invalid. It must be greater than zero.");
+    }
+}
---
+++ src/Domain/Todo/Events/TodoItemCreatedEvent.cs
@@ -0,0 +1,10 @@
+﻿using RuanFa.Shop.Domain.Todo.Entities;
+using RuanFa.Shop.SharedKernel.Models;
+
+namespace RuanFa.Shop.Domain.Todo.Events;
+public sealed class TodoItemCreatedEvent : DomainEvent
+{
+    public TodoItemCreatedEvent(TodoItem item) => Item = item;
+
+    public TodoItem Item { get; }
+}
